fa7e0513d36c5b482fd7cb08eb35ed15f7dd39ee
Emmanuele <emagnubcp@gmail.com>
2024-05-27 15:21:37 +0100

    15:00 - Add model classes

--- lib/core_src/presentation/pages/home_page.dart
+++ lib/core_src/presentation/pages/home_page.dart
@@ -11,42 +11,42 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(title: const Text('Home Page'),),
-      body: _buildUI(context),
-    );
+    return const Scaffold(
+        // appBar: AppBar(title: const Text('Home Page'),),
+        // body: _buildUI(context),
+        );
   }
 
-  Widget _buildUI(BuildContext context) {
-    return SafeArea(
-      child: SingleChildScrollView(
-        child: Container(
-          width: MediaQuery.sizeOf(context).width,
-          padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.sizeOf(context).width * 0.02),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              _allPokemonsList(context),
-            ],
-          ),
-        ),
-      ),
-    );
-  } //  End of _buildUI
+  // Widget _buildUI(BuildContext context) {
+  //   return SafeArea(
+  //     child: SingleChildScrollView(
+  //       child: Container(
+  //         width: MediaQuery.sizeOf(context).width,
+  //         padding: EdgeInsets.symmetric(
+  //             horizontal: MediaQuery.sizeOf(context).width * 0.02),
+  //         child: Column(
+  //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: <Widget>[
+  //             _allPokemonsList(context),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // } //  End of _buildUI
 
-  Widget _allPokemonsList(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.sizeOf(context).width,
-      child: const Column(
-        children: <Widget>[
-          Text('All Pokemons', style: TextStyle(fontSize: 25)),
-          Text('text')
-        ],
-      ),
-    );
-  }
+  // Widget _allPokemonsList(BuildContext context) {
+  //   return SizedBox(
+  //     width: MediaQuery.sizeOf(context).width,
+  //     child: const Column(
+  //       children: <Widget>[
+  //         Text('All Pokemons', style: TextStyle(fontSize: 25)),
+  //         Text('text')
+  //       ],
+  //     ),
+  //   );
+  // }
 
 //
 }  // End of HomeaPage()
